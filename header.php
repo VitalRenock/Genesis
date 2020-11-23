6e601cd733a8134161bc85c740b1546d6454ad71
VitalRenock <55354524+VitalRenock@users.noreply.github.com>
2020-11-24 00:35:32 +0100

    Update css du menu header

--- header.php
+++ header.php
@@ -1,3 +1,12 @@
 <header>
-    <h1>Mexo et Renock codent!!!</h1>
+    <div class="menu_global">
+        <h1>Mexo et Renock codent!</h1>
+        <nav>
+            <ul>
+                <li><a href="/index.php">Accueil</a></li>
+                <li><a href="/mexojer.php">Mexojer</a></li>
+                <li><a href="/renock.php">Renock</a></li>
+            </ul>
+        </nav>
+    </div>
 </header>
