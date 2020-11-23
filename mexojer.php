c4434df598423b86cc225b888ee91707ac3c0724
Thunus <Sephiroth_Strife880@hotmail.com>
2020-11-23 23:48:27 +0100

    ajout Bootstrap

--- mexojer.php
+++ mexojer.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" media="screen" href="/css-mexo/style.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Long+Cang|Montserrat:300,400,700&display=swap" rel="stylesheet">
     <title>Mexojer</title>
 </head>
 <body>
-    <a href="/index.html">Retour</a>
+    <div class="menu">
+        <h1>Mexojer</h1>
+        <nav>
+            <ul>
+                <li><a href="/mexojer.html">Mexojer</a></li>
+                <li><a href="/renock.html">Renock</a></li>
+                <li><a href="/index.html">Retour</a></li>
+            </ul>
+        </nav>
+    </div>
+
+
 </body>
 </html>
