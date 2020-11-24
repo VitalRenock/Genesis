a67fcc8eb1cd9a564c69fab665d8befb2c6a4d49
VitalRenock <55354524+VitalRenock@users.noreply.github.com>
2020-11-24 02:03:45 +0100

    Update renock

--- balisesTexte.php
+++ balisesTexte.php
@@ -1,50 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+
+    <!-- Metas -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Css -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" media="screen" href="/css-global/style.css" type="text/css">
+    <link rel="stylesheet" media="screen" href="/css-renock/style_renock.css" type="text/css">
+
+    <!-- Titre -->
     <title>Les balises textes</title>
+
 </head>
+
 <body>
 
+    <!-- Header -->
     <?php include("header.php"); ?>
 
-    <ol>
-        <li>Les titres</li>
-        <h1>Titre en H1</h1>
-        <h2>Titre en H2</h2>
-        <h3>Titre en H3</h3>
-        <h4>Titre en H4</h4>
-        <h5>Titre en H5</h5>
-        <h6>Titre en H6</h6>
-
-        <li>Le paragraphe</li>
-        <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.<br> 
-            Eius, ex doloremque nemo explicabo obcaecati earum possimus<br> 
-            perspiciatis officiis, nisi molestias libero totam numquam<br> 
-            nobis natus iusto error? Perferendis, saepe quo.
-        </p>
-
-        <li>La liste simple</li>
-        <ul>
-            <li>Fraise</li>
-            <li>Orange</li>
-            <li>Kiwi</li>
-        </ul>
-        <br>
-        <li>La liste ordonnée</li>
+    <div class="TextRenock">
         <ol>
-            <li>Éplucher les fruits.</li>
-            <li>Couper les fruits en petits morceaux.</li>
-            <li>Placer les fruits coupés dans un grand plat.</li>
-            <li>Ajouter de la sangria et des glacons.</li>
+            <li>Les titres</li>
+            <h1>Titre en H1</h1>
+            <h2>Titre en H2</h2>
+            <h3>Titre en H3</h3>
+            <h4>Titre en H4</h4>
+            <h5>Titre en H5</h5>
+            <h6>Titre en H6</h6>
+    
+            <li>Le paragraphe</li>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.<br> 
+                Eius, ex doloremque nemo explicabo obcaecati earum possimus<br> 
+                perspiciatis officiis, nisi molestias libero totam numquam<br> 
+                nobis natus iusto error? Perferendis, saepe quo.
+            </p>
+    
+            <li>La liste simple</li>
+            <ul>
+                <li>Fraise</li>
+                <li>Orange</li>
+                <li>Kiwi</li>
+            </ul>
+            <br>
+            <li>La liste ordonnée</li>
+            <ol>
+                <li>Éplucher les fruits.</li>
+                <li>Couper les fruits en petits morceaux.</li>
+                <li>Placer les fruits coupés dans un grand plat.</li>
+                <li>Ajouter de la sangria et des glacons.</li>
+            </ol>
         </ol>
-    </ol>
+    </div>
 
+    <!-- Footer -->
     <?php include("footer.php"); ?>
 
 </body>
+
 </html>
--- renock.php
+++ renock.php
@@ -1,22 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+
+    <!-- Metas -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" media="screen" href="/css-global/style.css" type="text/css">
     <link rel="stylesheet" media="screen" href="/css-renock/style_renock.css" type="text/css">
+
+    <!-- Titre -->
     <title>Renock</title>
 </head>
+
 <body>
 
+    <!-- Header -->
     <?php include("header.php"); ?>
 
     <div class="BlockRenock">
         <a href="/balisesTexte.php">Les balises de texte</a><br>
     </div>
 
-
+    <!-- Footer -->
     <?php include("footer.php"); ?>
 </body>
+
 </html>
