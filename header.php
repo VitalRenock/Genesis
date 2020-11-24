ac1c7a074a9eee0bd679a396c7e7307fd64a8d39
Thunus <Sephiroth_Strife880@hotmail.com>
2020-11-24 02:06:18 +0100

    modif CSS

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <header>
     <h1>Mexo et Renock codent!</h1>
-    <div class="menu_global">
-        <nav>
-            <ul>
-                <li><a href="/index.php">Accueil</a></li>
-                <li><a href="/mexojer.php">Mexojer</a></li>
-                <li><a href="/renock.php">Renock</a></li>
-            </ul>
-        </nav>
-    </div>
 </header>
+<div class="menu_global">
+    <nav>
+        <ul>
+            <li><a href="/index.php">Accueil</a></li>
+            <li><a href="/mexojer.php">Mexojer</a></li>
+            <li><a href="/renock.php">Renock</a></li>
+        </ul>
+    </nav>
+</div>
--- mexojer.php
+++ mexojer.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" media="screen" href="/css-mexo/style.css" type="text/css">
+    <link rel="stylesheet" media="screen" href="/css-global/style.css" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Long+Cang|Montserrat:300,400,700&display=swap" rel="stylesheet">
     <title>Mexojer</title>
 </head>
